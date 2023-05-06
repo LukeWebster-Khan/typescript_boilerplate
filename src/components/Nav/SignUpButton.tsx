ba48a517d0769f835f2642dd840cf8bb0b56f412
LukeWebsterKhan <lukewebsterkhan@gmail.com>
2023-05-06 22:24:40 +0100

    feat: init SignUpButton

---
+++ src/components/Nav/SignUpButton.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SignUpButton = () => {
+    return (
+        <button
+            color="primary"
+        >
+
+            Sign Up
+        </button>
+    );
+};
+export default SignUpButton;
