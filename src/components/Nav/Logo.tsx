6386c61b46efa95086027c49a78ba5af982f3a56
LukeWebsterKhan <lukewebsterkhan@gmail.com>
2023-05-06 14:49:07 +0100

    feat: init logo component

---
+++ src/components/Nav/Logo.tsx
@@ -0,0 +1,11 @@
+import exp from 'constants';
+import React from 'react';
+import './scss/_Nav.scss'
+
+const Logo = () => {
+    return (
+        <div className="Nav">
+        </div>
+    );
+};
+export default Logo;
