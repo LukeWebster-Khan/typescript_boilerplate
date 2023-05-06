1c3e240621cb659258fc46ea619e5a733f5d8877
LukeWebsterKhan <lukewebsterkhan@gmail.com>
2023-05-06 14:09:30 +0100

    feat: added header component and refactor

--- src/index.tsx
+++ src/index.tsx
@@ -14,14 +14,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
