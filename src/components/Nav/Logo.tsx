59fcd4175d4b2486f7a0176d062ed97ef6a15f86
LukeWebsterKhan <lukewebsterkhan@gmail.com>
2023-05-06 22:24:23 +0100

    feat: styled strapline

--- src/components/Nav/Logo.tsx
+++ src/components/Nav/Logo.tsx
@@ -3,8 +3,9 @@ import GoogleLogo from '../../google_icon.png';
 import './Nav.css'
 const Logo = () => {
     return (
-        <div className="Nav__logo">
+        <div className="Nav__logo Nav__section">
             <img src={GoogleLogo} alt="Google" />
+            <p  className='Nav__strapline'>Made with ❤️ by pet parents</p>
         </div>
     );
 };
